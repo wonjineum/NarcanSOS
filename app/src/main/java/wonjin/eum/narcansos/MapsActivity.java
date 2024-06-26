b0d7e2af84bed9253df7b8d10954ac67f0921c59
kunyoungPark <p3145@daum.net>
2024-06-27 08:15:48 +0900

    kyk

--- app/src/main/java/wonjin/eum/narcansos/MapsActivity.java
+++ app/src/main/java/wonjin/eum/narcansos/MapsActivity.java
@@ -1,8 +1,17 @@
 package wonjin.eum.narcansos;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +23,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 
@@ -21,6 +32,34 @@ import wonjin.eum.narcansos.databinding.ActivityMapsBinding;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    // Declare the launcher at the top of your Activity/Fragment:
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                }
+            });
+
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(POST_NOTIFICATIONS);
+            }
+        }
+    }
+
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -47,6 +86,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
 //        latlngs.add(new LatLng(12.334343, 33.43434)); //some latitude and logitude value
@@ -59,6 +99,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //            googleMap.addMarker(options);
 //        }
 
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
     }
 
     @Override
@@ -66,15 +107,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(12.356343, 33.56644);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng first = new LatLng(36.5972, 121.8944);
+        mMap.addMarker(new MarkerOptions().position(first).title("First"));
+        LatLng second = new LatLng(36.5972, 121.8944);
+        mMap.addMarker(new MarkerOptions().position(second).title("second"));
+        LatLng third = new LatLng(36.5972, 121.8944);
+        mMap.addMarker(new MarkerOptions().position(third).title("third"));
 
-        /*
-           1. 36.5972° N, 121.8978° W
-   2. 36.5972° N, 121.8944° W
-   3. 36.5984° N, 121.8879° W
-         */
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+
+    //    latlngs.add(new LatLng(36.5972, 121.8978)); //some latitude and logitude value
+    //    latlngs.add(new LatLng(36.5984, 121.8879));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(first));
         mMap.setOnMarkerClickListener(this);
     }
 
--- app/src/main/java/wonjin/eum/narcansos/Requesting.java
+++ app/src/main/java/wonjin/eum/narcansos/Requesting.java
@@ -4,25 +4,43 @@ import static android.Manifest.permission.CALL_PHONE;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Requesting extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.requesting);
 
+        Button todb = findViewById(R.id.button_help);
         Button call = findViewById(R.id.button_help2);
+
         call.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,5 +59,46 @@ public class Requesting extends AppCompatActivity {
             }
         });
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Toast.makeText(Requesting.this, "Location: "+location.toString(),Toast.LENGTH_LONG ).show();
+
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                        }
+                    }
+                });
+
+        todb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                Map<String, Object> user = new HashMap<>();
+                user.put("first", "Ada");
+                user.put("last", "Lovelace");
+                user.put("born", 1815);
+
+// Add a new document with a generated ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("good: ", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("bad: ", "Error adding document", e);
+                            }
+                        });
+
+            }
+        });
     }
 }
